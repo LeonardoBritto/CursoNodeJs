872713e65db72a3bcf7be2a24bd296863aabde2a
Leonardo Brito <leonardobrito992@gmail.com>
2022-10-10 23:28:50 -0300

    Fim Seção 01

---
+++ 01_INTRODUCAO/01_PRIMEIRO_PROGRAMA/Programa.js
@@ -0,0 +1,10 @@
+console.log("Patrikas")
+
+var a, b;
+
+a = 5*9;
+b = a + 5;
+
+console.log(b)
+
+/*PARA EXECUTAR ESTE PROGRAMA, VA AO TERMINAL E DIGITE NODE .\Programa.Js*/
---
+++ 01_INTRODUCAO/03_TAREFA/Programa.js
@@ -0,0 +1,4 @@
+const a = 15
+const b = 35
+
+console.log(a+b)
