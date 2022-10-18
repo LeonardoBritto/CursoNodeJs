32badefd2880b780ff27f581df0a9ce0cf0a6027
Leonardo <leonardobrito992@gmail.com>
2022-10-18 17:59:23 -0300

    Continuando...

--- 15_GET_A_PET/BACKEND/Index.js
+++ 15_GET_A_PET/BACKEND/Index.js
@@ -1,14 +1,9 @@
 const express = require('express')
 const cors = require('cors')
-const bodyParser = require('body-parser')
 
 const app = express()
 
-//Config JSON Response
-//app.use(express.json())
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.json())
 
 //Salve CORS
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
